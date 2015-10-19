7cff61afa02561751ceb3d7c84ce99d4ab8ec9f4
char-lie <5962274+char-lie@users.noreply.github.com>
2015-10-19 22:31:02 +0300

    Merged all constants to 'groups.sh'

--- lab1/groups.sh
+++ lab1/groups.sh
@@ -4,6 +4,8 @@ declare -A MY_GROUPS
 declare -A BASIC_PRIVILEGES
 declare -A ADDITIONAL_READ_PRIVILEGES
 
+declare -A MY_USERS
+
 MY_GROUPS=(
 ["alfa"]="w_gromov"
 ["beta"]="k_beglov b_rebrov n_kalinina"
@@ -24,4 +26,23 @@ ADDITIONAL_READ_PRIVILEGES=(
 ["nabla"]="beta"
 ["sigma"]="beta nabla"
 )
+
+MY_USERS=(
+# alfa
+["w_gromov"]="-p $(perl -e 'print crypt("$ARGV[0]", "ololo")' "123!@#][p}{P")"
+# beta
+["k_beglov"]="-e `date -d '+45 days' +%Y-%m-%d` -p $(perl -e 'print crypt("$ARGV[0]", "ololo")' "abcdefgh")"
+["b_rebrov"]="-e `date -d '+12 days' +%Y-%m-%d` -p $(perl -e 'print crypt("$ARGV[0]", "ololo")' "1b3d5f7h")"
+["n_kalinina"]="-e `date -d '+30 days' +%Y-%m-%d` -p $(perl -e 'print crypt("$ARGV[0]", "ololo")' "a2c4e6g8")"
+# nabla
+["i_frolov"]="-e `date -d '+10 days' +%Y-%m-%d` -p $(perl -e 'print crypt("$ARGV[0]", "ololo")' "qwertyas")"
+["d_lavrov"]="-e `date -d '+60 days' +%Y-%m-%d` -p $(perl -e 'print crypt("$ARGV[0]", "ololo")' "asqwerty")"
+["e_ivanova"]="-e `date -d '+30 days' +%Y-%m-%d` -p $(perl -e 'print crypt("$ARGV[0]", "ololo")' "12345678")"
+# sigma
+["t_uporov"]="-e `date -d '+15 days' +%Y-%m-%d` -p $(perl -e 'print crypt("$ARGV[0]", "ololo")' "ertyuiop")"
+["m_kruglov"]="-e `date -d '+30 days' +%Y-%m-%d` -p $(perl -e 'print crypt("$ARGV[0]", "ololo")' "poiuytre")"
+["r_klinova"]="-e `date -d '+90 days' +%Y-%m-%d` -p $(perl -e 'print crypt("$ARGV[0]", "ololo")' "12345678")"
+)
+
+ADMINISTRATION="w_gromov"
 
